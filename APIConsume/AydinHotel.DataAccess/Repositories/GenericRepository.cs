4a9eacc4dbecb434e6a0d826417ceec5ba915eb7
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-11-04 13:54:22 +0300

    Entity Framework Class'ları için GenericRepository sınıfı yazıldı.

---
+++ APIConsume/AydinHotel.DataAccess/Repositories/GenericRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AydinHotel.DataAccess.Abstracts;
+using AydinHotel.DataAccess.Concrete;
+using AydinHotel.Entity.Abstracts;
+
+namespace AydinHotel.DataAccess.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class, IEntity, new()
+    {
+        private readonly Context _context;
+
+        public GenericRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public void Insert(T entity)
+        {
+            _context.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            _context.Update(entity);
+            _context.SaveChanges();
+        }
+
+        public List<T> GetList()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+    }
+}
