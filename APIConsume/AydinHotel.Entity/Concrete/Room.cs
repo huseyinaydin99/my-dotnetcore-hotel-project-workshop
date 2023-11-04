8581b0d2e8a4a1a91a74b8b29fce7245890a8166
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-11-04 12:41:46 +0300

    APIConsume için tüm katmanlar oluşturuldu. Entity katmanında ORM için Room, Service, Staff, Testimonial varlık sınıfları oluşturuldu.

--- APIConsume/AydinHotel.Entity/Concrete/Room.cs
+++ APIConsume/AydinHotel.Entity/Concrete/Room.cs
@@ -8,6 +8,14 @@ namespace AydinHotel.Entity.Concrete
 {
     public class Room
     {
-
+        public int RoomId { get; set; }
+        public string RoomNumber { get; set; }
+        public string RoomCoverImage { get; set; }
+        public int Price { get; set; }
+        public string Title { get; set; }
+        public string BedCount { get; set; }
+        public string BathCount { get; set; }
+        public string Wifi { get; set; }
+        public string Description { get; set; }
     }
 }
