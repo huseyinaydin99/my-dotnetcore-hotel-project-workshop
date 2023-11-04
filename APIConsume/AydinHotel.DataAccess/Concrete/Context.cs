8d18bab8dca301e749f2c15f9e68e6f03fefa422
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-11-04 13:17:49 +0300

    Context sınıfı oluşturuldu, veritabanı bağlantısı yapıldı ve DbContext'ler tanımlandı.

---
+++ APIConsume/AydinHotel.DataAccess/Concrete/Context.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AydinHotel.Entity.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace AydinHotel.DataAccess.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server = DESKTOP-13123BI; Initial Catalog = AydinHotelAPIDB; Integrated Security = true;");
+        }
+
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Staff> Staffs { get; set; }
+        public DbSet<Subscribe> Subscribes { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
---
+++ APIConsume/AydinHotel.Entity/Concrete/Subscribe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AydinHotel.Entity.Concrete
+{
+    public class Subscribe
+    {
+        public int SubscribeId { get; set; }
+        public string Mail { get; set; }
+    }
+}
