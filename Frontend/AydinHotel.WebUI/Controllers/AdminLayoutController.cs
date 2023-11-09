a59b59046b68e45a64a75ffd2d953aaa5f0a6967
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-11-09 16:30:49 +0300

    PartialView'ler halledildi. Partial'ler AdminLayoutController tarafında yazıldı. Tasarım parçalara ayrıldı çünkü hem düzenli kod(clean code) olması için hemde dilediğimiz bir partiali istediğimiz bir yerde tekrar kullanabilmemiz için.

---
+++ Frontend/AydinHotel.WebUI/Controllers/AdminLayoutController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AydinHotel.WebUI.Controllers
+{
+    public class AdminLayoutController : Controller
+    {
+        public IActionResult _AdminLayout()
+        {
+            return View();
+        }
+
+        public PartialViewResult HeadPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PreloaderPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult NavheaderPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult HeaderPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult SidebarPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult FooterPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult ScriptPartial()
+        {
+            return PartialView();
+        }
+    }
+}
