4eaed204700e02d9fbf9e99157f18d333ce41e33
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-11-09 20:22:29 +0300

    HttpClient configuration'ı yapıldı. StaffViewModel nesnesi yazıldı. Staff Index View'i tasarlandı.

--- Frontend/AydinHotel.WebUI/Controllers/StaffController.cs
+++ Frontend/AydinHotel.WebUI/Controllers/StaffController.cs
@@ -1,11 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AydinHotel.WebUI.Models.Staff;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace AydinHotel.WebUI.Controllers
 {
     public class StaffController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public StaffController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:21023/api/Staff");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<StaffViewModel>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
