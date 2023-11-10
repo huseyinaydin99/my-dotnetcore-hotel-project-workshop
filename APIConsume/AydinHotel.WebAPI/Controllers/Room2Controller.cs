aaec0a36ad15e849635329703846ccb9e3dd8605
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-11-10 13:23:07 +0300

    Room2Controller test amaçlı yazıldı. DTO'dan Entity'e - Entity'den ise DTO'a eşleme(dönüşüm) yapıldı. Validasyon testi çalışıyor.

---
+++ APIConsume/AydinHotel.WebAPI/Controllers/Room2Controller.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using AydinHotel.Business.Abstracts;
+using AydinHotel.DTOs.DTOs.RoomDTOs;
+using AydinHotel.Entity.Concretes;
+using Microsoft.AspNetCore.Mvc;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace AydinHotel.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Room2Controller : ControllerBase
+    {
+        private readonly IRoomService _roomService;
+        private readonly IMapper _mapper;
+        public Room2Controller(IRoomService roomService, IMapper mapper)
+        {
+            _roomService = roomService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok(_roomService.TGetList());
+        }
+
+        [HttpPost]
+        public IActionResult AddRoom(RoomAddDTO roomAddDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var values = _mapper.Map<Room>(roomAddDto);
+            _roomService.TInsert(values);
+            return Ok("Oda kaydı yapıldı.");
+        }
+
+        [HttpPost]
+        public IActionResult UpdateRoom(RoomUpdateDTO roomUpdateDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var values = _mapper.Map<Room>(roomUpdateDto);
+            _roomService.TUpdate(values);
+            return Ok("Oda başarı ile güncellendi.");
+        }
+    }
+}
