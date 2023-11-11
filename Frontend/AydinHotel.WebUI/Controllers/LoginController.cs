50460c428e60a85f82456e3faeff524f15a7fbe3
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-11-11 16:37:46 +0300

    Login işlemleri için Automapper configuration'u, LoginUserDTO sınıfı ve LoginController yazıldı.  LoginController'de SignInManager sınıfı kullanıldı.

---
+++ Frontend/AydinHotel.WebUI/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using AydinHotel.Entity.Concretes;
+using AydinHotel.WebUI.DTOs.LoginDTOs;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AydinHotel.WebUI.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public LoginController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(LoginUserDTO loginUserDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var result =
+                    await _signInManager.PasswordSignInAsync(loginUserDto.Username, loginUserDto.Password, false,
+                        false);
+                return result.Succeeded ? RedirectToAction("Index", "Staff") : View();
+            }
+            return View();
+        }
+    }
+}
---
+++ Frontend/AydinHotel.WebUI/DTOs/LoginDTOs/LoginUserDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AydinHotel.WebUI.DTOs.LoginDTOs
+{
+    public class LoginUserDTO
+    {
+        [Required(ErrorMessage = "Kullanıcı adı alanı zorunludur.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Şifre alanı zorunludur.")]
+        public string Password { get; set; }
+    }
+}
