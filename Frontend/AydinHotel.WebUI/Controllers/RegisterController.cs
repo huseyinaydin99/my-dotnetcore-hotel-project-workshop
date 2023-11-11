8624cc12049be5c0baa69e4f0e604d49b15bf8b6
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-11-11 13:31:21 +0300

    Register işlemi için RegisterDTO sınıfı yazıldı.

--- Frontend/AydinHotel.WebUI/Controllers/RegisterController.cs
+++ Frontend/AydinHotel.WebUI/Controllers/RegisterController.cs
@@ -1,10 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AydinHotel.Entity.Concretes;
+using AydinHotel.WebUI.DTOs.RegisterDTOs;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AydinHotel.WebUI.Controllers
 {
     public class RegisterController : Controller
     {
+        private UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(CreateNewUserDTO createNewUserDto)
         {
             return View();
         }
---
+++ Frontend/AydinHotel.WebUI/DTOs/RegisterDTOs/CreateNewUserDTO.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AydinHotel.WebUI.DTOs.RegisterDTOs
+{
+    public class CreateNewUserDTO
+    {
+        [Required(ErrorMessage = "Ad alanı gereklidir.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Soyad alanı gereklidir.")]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Kullanıcı adı alanı gereklidir.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "E-posta alanı gereklidir.")]
+        public string Mail { get; set; }
+
+        [Required(ErrorMessage = "Şifre alanı gereklidir.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Şifre tekrarı alanı gereklidir.")]
+        [Compare("Password", ErrorMessage = "Şifreler eşleşmiyor. Lütfen şifre ve şifre tekrarı alanlarını aynı giriniz.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
