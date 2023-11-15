f630c2b976d4eee9ce7add2e9b2a0ff8e7e05b23
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-11-15 15:36:53 +0300

    Odalarımız kısmı bitti. _OutRoomsPartial View'i yazıldı. _OurRoomsPartial sınıfı yazıldı ve Default Index'inde çağrıldı. Rooms tablosundaki veriler eklendi.

--- Frontend/AydinHotel.WebUI/ViewComponent/Default/_AboutUsPartial.cs
+++ Frontend/AydinHotel.WebUI/ViewComponent/Default/_AboutUsPartial.cs
@@ -1,11 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AydinHotel.WebUI.DTOs.ServiceDTO;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AydinHotel.WebUI.DTOs.AboutDTOs;
 
 namespace AydinHotel.WebUI.ViewComponent.Default
 {
     public class _AboutUsPartial : Microsoft.AspNetCore.Mvc.ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _AboutUsPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:21023/api/About");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAboutDTO>>(jsonData);
+                return View(values);
+            }
+
             return View();
         }
     }
--- Frontend/AydinHotel.WebUI/ViewComponent/Default/_OurRoomsPartial.cs
+++ Frontend/AydinHotel.WebUI/ViewComponent/Default/_OurRoomsPartial.cs
@@ -1,11 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AydinHotel.WebUI.DTOs.ServiceDTO;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AydinHotel.WebUI.DTOs.RoomDTOs;
 
 namespace AydinHotel.WebUI.ViewComponent.Default
 {
     public class _OurRoomsPartial : Microsoft.AspNetCore.Mvc.ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _OurRoomsPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:21023/api/Room");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultRoomDTO>>(jsonData);
+                return View(values);
+            }
+
             return View();
         }
     }
