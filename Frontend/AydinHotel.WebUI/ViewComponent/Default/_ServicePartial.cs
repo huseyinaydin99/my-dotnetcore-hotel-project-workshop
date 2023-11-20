3699220ab1e7355c681ec2312e2c156b122b88b0
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-11-20 14:20:30 +0300

    Trailer ve Servisler partialleri düzenlendi.

--- Frontend/AydinHotel.WebUI/ViewComponent/Default/_ServicePartial.cs
+++ Frontend/AydinHotel.WebUI/ViewComponent/Default/_ServicePartial.cs
@@ -1,12 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AydinHotel.WebUI.DTOs.RoomDTOs;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace AydinHotel.WebUI.ViewComponent.Default
 {
     public class _ServicePartial : Microsoft.AspNetCore.Mvc.ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _ServicePartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:21023/api/Service");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultRoomDTO>>(jsonData);
+                return View(values);
+            }
+
             return View();
         }
     }
