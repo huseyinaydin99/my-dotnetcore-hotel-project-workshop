489227d043efd09df96ef116edcec555c1311472
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-12-14 18:23:24 +0300

    RapidAPI'den Exchange verilerini çektim. RapidAPI kullanabiliyorum.

--- RapidAPI/API.Consume/Controllers/ExchangeController.cs
+++ RapidAPI/API.Consume/Controllers/ExchangeController.cs
@@ -1,16 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Net.Http.Headers;
 using System.Net.Http;
 using System;
 using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 using API.Consume.Models;
-using System.Text.Json.Serialization;
-using Newtonsoft.Json;
+using System.Linq;
 
 namespace API.Consume.Controllers
 {
-    public class ImdbController : Controller
+    public class ExchangeController : Controller
     {
         public async Task<IActionResult> Index()
         {
@@ -18,19 +18,19 @@ namespace API.Consume.Controllers
             var request = new HttpRequestMessage
             {
                 Method = HttpMethod.Get,
-                RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/series/"),
+                RequestUri = new Uri("https://booking-com.p.rapidapi.com/v1/metadata/exchange-rates?currency=AED&locale=en-gb"),
                 Headers =
                 {
                     { "X-RapidAPI-Key", "dd3fa5539bmsh3b20c335e1895adp104c66jsn47d0556c5e5e" },
-                    { "X-RapidAPI-Host", "imdb-top-100-movies.p.rapidapi.com" },
+                    { "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
                 },
             };
             using (var response = await client.SendAsync(request))
             {
                 response.EnsureSuccessStatusCode();
                 var body = await response.Content.ReadAsStringAsync();
-                var apiMovieViewModels = JsonConvert.DeserializeObject<List<ApiMovieViewModel>>(body);
-                return View(apiMovieViewModels);
+                var values = JsonConvert.DeserializeObject<ExchangeViewModel>(body);
+                return View(values.exchange_rates.ToList());
             }
             return View();
         }
