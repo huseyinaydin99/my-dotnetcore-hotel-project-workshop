db84d7047b8a08eade3c78c70160cef4153fb9fe
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-03-22 14:08:55 +0300

    AdminContactController ve Inbox view'i eklendi. Inbox view'i üzerinde tasarımsal düzenlemeler yapıldı.

---
+++ Frontend/AydinHotel.WebUI/Controllers/AdminContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AydinHotel.WebUI.Controllers
+{
+    public class AdminContactController : Controller
+    {
+        public IActionResult Inbox()
+        {
+            return View();
+        }
+    }
+}
