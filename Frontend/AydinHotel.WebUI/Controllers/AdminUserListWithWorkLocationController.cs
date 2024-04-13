073cfad7bbc67cbf73c8d5bb8ee2c3c19e83a8df
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-04-13 12:27:50 +0300

    Kullanıcılar ile çalışma lokasyonları joinlendi ve listelendi. .Kullanıcıları lokasyonları ile getirme kısmı tamamlandı.

---
+++ Frontend/AydinHotel.WebUI/Controllers/AdminUserListWithWorkLocationController.cs
@@ -0,0 +1,34 @@
+﻿using AydinHotel.WebUI.DTOs.AppUserDTOs;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AydinHotel.WebUI.Controllers
+{
+    public class AdminUserListWithWorkLocationController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminUserListWithWorkLocationController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> UserList()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:21023/api/AppUserWorkLocation");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAppUserWithWorkLocationDTO>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
